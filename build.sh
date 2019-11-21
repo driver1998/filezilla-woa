5a505e4b594d1ba40fb8503a6f2fb6ff97c49900
GH Cao <driver1998@foxmail.com>
2019-11-22 04:22:38 +0800

    v3.45.1 ready

--- build.sh
+++ build.sh
@@ -1,55 +1,105 @@
-#!/usr/bin/env bash
 
-ARCH=aarch64
 HOST=$ARCH-w64-mingw32
-PREFIX=$PWD/filezilla-$HOST
-PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+PREFIX=$PWD/$HOST
+PREFIX_AUTOMAKE=$PWD/automake
 
-MINGW_ROOT=$PWD/llvm-mingw
-PATH=$PATH:$MINGW_ROOT/bin
-LDFLAGS="-L$MINGW_ROOT/lib/clang/9.0.0/lib/windows -lclang_rt.builtins-aarch64"
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX/lib
+export MINGW_ROOT=$PWD/$(find llvm-mingw* -type d | head -n 1)
+export PATH=$PATH:$MINGW_ROOT/bin
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
+
+export CFLAGS="-Wno-unused-function -Wno-unused-lambda-capture \
+               -Wno-unused-variable -Wno-ignored-attributes \
+               -Wno-inconsistent-missing-override -Wno-inconsistent-dllimport"
+export CXXFLAGS=$CFLAGS
+
+echo Building for $HOST
+
+# build libpowrprof.a
+(
+    echo LIBRARY POWRPROF.dll
+    echo EXPORTS
+    $HOST-nm $MINGW_ROOT/i686-w64-mingw32/lib/libpowrprof.a --just-symbol-name |
+        sed '/^$/d' | sed '/^POWRPROF.dll/d' | sed '/\.idata/d' | sed '/__imp/d' | sed '/__NULL_IMPORT/d' | 
+        sed '/_NULL_THUNK_DATA/d' | sed '/__IMPORT_DESCRIPTOR/d' | sed 's/@.*$//g' | sed 's/^_//g'
+) > powrprof.def
+$HOST-dlltool -d powrprof.def -l $MINGW_ROOT/$HOST/lib/libpowrprof.a
+
+# automake
+pushd $(find automake* -type d | head -n 1)
+./configure --prefix=$PREFIX_AUTOMAKE || exit 1
+make || exit 1
+make install || exit 1
+export PATH=$PREFIX_AUTOMAKE/bin:$PATH
+popd
 
 # gmp
-pushd gmp
-./configure --prefix=$(PREFIX) --host=$(HOST) --disable-static --enable-shared --disable-cxx --disable-assembly || exit 1
-make install -j8 || exit 1
+pushd $(find gmp* -type d | head -n 1)
+patch -i ../gmp-aarch64-configure-tweaks.patch
+autoconf
+./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared --disable-cxx --disable-assembly || exit 1
+make -j $(nproc) || exit 1
+make install     || exit 1
 popd
 
 # nettle
-pushd nettle
-./configure --prefix=$(PREFIX) --host=$(HOST) --disable-static --enable-shared --disable-assembler || exit 1
-make install -j8 || exit 1
+pushd $(find nettle* -type d | head -n 1)
+./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared --disable-assembler \
+            --with-include-path=$PREFIX/include --with-lib-path=$PREFIX/lib || exit 1
+make -j $(nproc) || exit 1
+make install     || exit 1
 popd
 
 # gnutls
-pushd gnutls
-./configure --prefix=$(PREFIX) --host=$(HOST) --disable-static --enable-shared --without-p11-kit --with-included-libtasn1 --with-included-unistring --enable-local-libopts --disable-srp-authentication --disable-dtls-srtp-support --disable-heartbeat-support --disable-psk-authentication --disable-anon-authentication --disable-openssl-compatibility --without-tpm --disable-cxx --disable-hardware-acceleration --disable-tools --disable-doc || exit 1
-make install -j8 || exit 1
+pushd $(find gnutls* -type d | head -n 1)
+GMP_LIBS="-L$PREFIX/lib -lgmp" \
+./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared --disable-cxx \
+            --without-p11-kit --with-included-libtasn1 --with-included-unistring --enable-local-libopts \
+            --disable-srp-authentication --disable-dtls-srtp-support --disable-heartbeat-support \
+            --disable-psk-authentication --disable-anon-authentication --disable-openssl-compatibility --without-tpm \
+            --disable-hardware-acceleration --disable-tools --disable-doc || exit 1
+make -j $(nproc) || exit 1
+make install     || exit 1
 popd
 
 # sqlite
-pushd sqlite
-./configure --prefix=$(PREFIX) --host=$(HOST) --disable-static --enable-shared || exit 1
-make install -j8 || exit 1
+pushd $(find sqlite* -type d | head -n 1)
+./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared || exit 1
+make         || exit 1
+make install || exit 1
 popd
 
 # libfilezilla
-#curl 'https://dl1.cdn.filezilla-project.org/libfilezilla/libfilezilla-0.18.1.tar.bz2?h=wbiDjBgwuLHoc6O_BRRFPA&x=1566413717' -o libfilezilla.tar.bz2 || exit 1
-#tar -xjf libfilezilla.tar.bz2 || exit 1
-#cd libfilezilla
-#./configure --prefix=$(PREFIX) --host=$(HOST) --disable-shared --disable-doxygen-doc
-
-# wxwidgets
-pushd wxwidgets
-./configure --prefix=$(PREFIX) --host=$(HOST) --disable-static --enable-shared --enable-monolithic --without-opengl || exit 1
-make install -j8 || exit 1
+pushd $(find libfilezilla* -type d | head -n 1)
+./configure --prefix=$PREFIX --host=$HOST --disable-shared --disable-doxygen-doc || exit 1
+make -j $(nproc) || exit 1
+make install     || exit 1
 popd
 
 # filezilla
-#curl https://dl3.cdn.filezilla-project.org/client/FileZilla_3.44.2_src.tar.bz2?h=cXaccyaUk41EyGl4z04-cQ&x=1566413973 -o filezilla.tar.bz2 || exit 1
-#tar -xjf filezilla.tar.bz2 || exit 1
-#cd filezilla
-#./configure --prefix=$(PREFIX) --host=$(HOST) --disable-shellext --with-wx-config=$(PREFIX)/bin/wx-config
+pushd $(find filezilla* -type d | head -n 1)
+sed -i -r 's/("\$ac_abs_confdir.*--host=i686.*$)/LDFLAGS="-L$MINGW_ROOT\/lib\/clang\/9.0.0\/lib\/windows -lclang_rt.builtins-i386" \1/g' configure.ac
+sed -i -r 's/("\$ac_abs_confdir.*--host=x86_64.*$)/LDFLAGS="-L$MINGW_ROOT\/lib\/clang\/9.0.0\/lib\/windows -lclang_rt.builtins-x86_64" \1/g' configure.ac
+autoconf
 
-find $PWD/filezilla-$HOST
+pushd src/fzshellext
+sed -i 's/target:\.\*mingw\./target:\.\*mingw\\\|windows\./g' configure.ac
+autoconf
+popd
+
+./configure --prefix=$PREFIX --host=$HOST --with-wx-config=$PREFIX/bin/wx-config || exit 1
+make -j $(nproc) || exit 1
+make install     || exit 1
+popd
+
+mkdir output
+cp -R $PREFIX/bin/filezilla.exe \
+      $PREFIX/bin/fzputtygen.exe \
+      $PREFIX/bin/fzsftp.exe \
+      $PREFIX/lib/wxmsw30u_gcc_custom.dll \
+      $PREFIX/share/filezilla/* \
+      $PREFIX/bin/*.dll \
+      $MINGW_ROOT/$HOST/bin/*.dll output
+
+pushd output
+$HOST-strip *.dll *.exe
+popd

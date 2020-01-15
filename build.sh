a202dfc1afe418678967a89fac572d3a0015c5ae
GH Cao <driver1998@foxmail.com>
2020-01-15 20:57:31 +0800

    v3.46.3 testing

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ PREFIX=$PWD/$HOST
 PREFIX_AUTOMAKE=$PWD/automake
 
 export MINGW_ROOT=$PWD/$(find llvm-mingw* -type d | head -n 1)
-export PATH=$PATH:$MINGW_ROOT/bin
+export PATH=$PREFIX_AUTOMAKE/bin:$PATH:$MINGW_ROOT/bin:$PREFIX/bin
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig
 
 export CFLAGS="-Wno-unused-function -Wno-unused-lambda-capture -Wno-unused-variable -Wno-ignored-attributes -Wno-inconsistent-missing-override -Wno-inconsistent-dllimport -O2"
@@ -17,7 +17,7 @@ echo Building for $HOST
     echo LIBRARY POWRPROF.dll
     echo EXPORTS
     $HOST-nm $MINGW_ROOT/i686-w64-mingw32/lib/libpowrprof.a --just-symbol-name |
-        sed '/^$/d' | sed '/^POWRPROF.dll/d' | sed '/\.idata/d' | sed '/__imp/d' | sed '/__NULL_IMPORT/d' | 
+        sed '/^$/d' | sed '/^POWRPROF.dll/d' | sed '/\.idata/d' | sed '/__imp/d' | sed '/__NULL_IMPORT/d' |
         sed '/_NULL_THUNK_DATA/d' | sed '/__IMPORT_DESCRIPTOR/d' | sed 's/@.*$//g' | sed 's/^_//g'
 ) > powrprof.def
 $HOST-dlltool -d powrprof.def -l $MINGW_ROOT/$HOST/lib/libpowrprof.a
@@ -27,12 +27,11 @@ pushd $(find automake* -type d | head -n 1)
 ./configure --prefix=$PREFIX_AUTOMAKE || exit 1
 make || exit 1
 make install || exit 1
-export PATH=$PREFIX_AUTOMAKE/bin:$PATH
 popd
 
 # gmp
 pushd $(find gmp* -type d | head -n 1)
-patch -i ../gmp-aarch64-configure-tweaks.patch
+patch -i ../patches/gmp-aarch64-configure-tweaks.patch
 autoconf
 ./configure --prefix=$PREFIX --host=$HOST --disable-static --enable-shared --disable-cxx --disable-assembly || exit 1
 make -j $(nproc) || exit 1
@@ -68,6 +67,9 @@ popd
 
 # libfilezilla
 pushd $(find libfilezilla* -type d | head -n 1)
+for p in ../patches/libtool/*.patch; do patch -p1 < "$p"; done
+autoconf
+
 ./configure --prefix=$PREFIX --host=$HOST --disable-shared --disable-doxygen-doc || exit 1
 make -j $(nproc) || exit 1
 make install     || exit 1
@@ -75,29 +77,25 @@ popd
 
 # filezilla
 pushd $(find filezilla* -type d | head -n 1)
-sed -i -r 's/("\$ac_abs_confdir.*--host=i686.*$)/LDFLAGS="-L$MINGW_ROOT\/lib\/clang\/9.0.0\/lib\/windows -lclang_rt.builtins-i386" \1/g' configure.ac
-sed -i -r 's/("\$ac_abs_confdir.*--host=x86_64.*$)/LDFLAGS="-L$MINGW_ROOT\/lib\/clang\/9.0.0\/lib\/windows -lclang_rt.builtins-x86_64" \1/g' configure.ac
+for p in ../patches/libtool/*.patch;   do patch -p1 < "$p"; done
+for p in ../patches/filezilla/*.patch; do patch -p1 < "$p"; done
 autoconf
-
 pushd src/fzshellext
-sed -i 's/target:\.\*mingw\./target:\.\*mingw\\\|windows\./g' configure.ac
 autoconf
 popd
 
 ./configure --prefix=$PREFIX --host=$HOST --with-wx-config=$PREFIX/bin/wx-config || exit 1
 make -j $(nproc) || exit 1
 make install     || exit 1
-popd
 
-mkdir output
-cp -R $PREFIX/bin/filezilla.exe \
-      $PREFIX/bin/fzputtygen.exe \
-      $PREFIX/bin/fzsftp.exe \
-      $PREFIX/lib/wxmsw30u_gcc_custom.dll \
-      $PREFIX/share/filezilla/* \
-      $PREFIX/bin/*.dll \
-      $MINGW_ROOT/$HOST/bin/*.dll output
+$HOST-strip src/interface\filezilla.exe \
+            src/putty/fzsftp.exe \
+            src/putty/fzputtygen.exe \
+            src/fzshellext/32/libfzshellext-0.dll \
+            src/fzshellext/64/libfzshellext-0.dll \
+            data/dlls/*.dll
 
-pushd output
-$HOST-strip *.dll *.exe
+pushd data
+./makezip.sh
+mv FileZilla.zip ../../
 popd
